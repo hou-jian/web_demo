948453c6ae6cd4b07146853314011a493fe51aab
houji <hj1254123@gmail.com>
2023-03-28 15:34:23 +0800

    fix: 修复Router使用错误，并增加了一些注释。

--- 2023-3-26 Service Worker/Router.js
+++ 2023-3-26 Service Worker/Router.js
@@ -1,6 +1,9 @@
 // 在 fetch 事件回调当中主要进行着资源请求匹配和响应结果返回的操作，
 // 可以把这个过程当做一个路由分发的问题，
 // 因此我们可以封装一个 Router 类来实现对路由的匹配规则和操作分发的统一管理。
+// 注意 match 请求拦截 和 respond 请求响应，两个函数的说明，
+// 只是为了演示基本的封装逻辑，并没有考虑各种情况。
+// 在真实开发中建议使用 Workbox。
 class Router {
   constructor() {
     // 存放路由规则
@@ -11,6 +14,7 @@ class Router {
 
   initProxy() {
     self.addEventListener('fetch', event => {
+      console.log(event)
       // 当拦截到资源请求时，会遍历已注册的路由规则，并执行相应规则所对应的策略
       for(let route of this.routes) {
         // 使用前面封装好的 match 函数进行路由规则匹配
@@ -29,6 +33,8 @@ class Router {
 }
 
 // 资源请求判断
+// 这里为了简便，没有判断请求方法，默认你用的get，
+// 可以用 request.method 获取，在rule传入方法字段再检查。
 function match(rule, request) {
   switch(Object.prototype.toString.call(rule)) {
     // url 文本匹配
@@ -48,7 +54,8 @@ function match(rule, request) {
 }
 
 // 资源请求响应的错误处理
-// 错误返回 500状态码
+// 错误返回 500状态码，这里是为了简单才这样封装的
+// 可以封装为错误发送fetch请求真实的资源
 function respond(event, handler) {
   try {
     // 执行响应处理方法，根据返回结果进行兜底
--- 2023-3-26 Service Worker/sw.js
+++ 2023-3-26 Service Worker/sw.js
@@ -14,33 +14,36 @@ self.addEventListener('activate', () => {
   console.log('service worker 激活成功')
 })
 
-self.addEventListener('fetch', event => {
-  router.registerRoute(
-    '/data.txt',
-    () => new Response('hello txt')
-  )
-  router.registerRoute(
-    '/data.json',
-    () => new Response('hello json')
-  )
-  router.registerRoute(
-    '/user.json',
-    () => new Response('hello user')
-  )
-  // // 完整或相对URL匹配
-  // if(match('/data.txt', event.request)) {
-  //   return event.respondWith(new Response('完整或相对URL匹配成功'))
-  // }
-  // // 正则匹配
-  // if(match(/\/data\.json/, event.request)) {
-  //   return event.respondWith(new Response('正则匹配成功'))
-  // }
-  // // 自定义匹配
-  // if(match(
-  //   request => request.url.indexOf('/user.json') > 0,
-  //   event.request
-  // )) {
-  //   return event.respondWith(new Response('自定义匹配成功'))
-  // }
-})
+// 请求拦截与响应，请求拦截只支持get，响应出错返回500状态码
+router.registerRoute(
+  '/data.txt',
+  () => new Response('hello txt')
+)
+router.registerRoute(
+  '/data.json',
+  () => new Response('hello json')
+)
+router.registerRoute(
+  '/user.json',
+  () => new Response('hello user')
+)
+
+// self.addEventListener('fetch', event => {
+  
+//   // 完整或相对URL匹配
+//   if(match('/data.txt', event.request)) {
+//     return event.respondWith(new Response('完整或相对URL匹配成功'))
+//   }
+//   // 正则匹配
+//   if(match(/\/data\.json/, event.request)) {
+//     return event.respondWith(new Response('正则匹配成功'))
+//   }
+//   // 自定义匹配
+//   if(match(
+//     request => request.url.indexOf('/user.json') > 0,
+//     event.request
+//   )) {
+//     return event.respondWith(new Response('自定义匹配成功'))
+//   }
+// })
 
